e58248b5649028fd64595b733e457dacf210302f
Ofir Siboni <OfirSiboni@users.noreply.github.com>
2020-04-30 01:07:01 +0300

    added UDP protocol for config values

--- HawkEye/source/main.py
+++ HawkEye/source/main.py
@@ -43,6 +43,7 @@ height = 120
 fps = 30
 cameras = []
 grip_pipe = None
+prev,nex,conf,pt,done = False,False,False,False,False
 #paths
 __config_path__ = os.path.expanduser('~') + '/.hawk'
 
@@ -70,14 +71,18 @@ __instance__.startClientTeam(team_number)
 __pipeline__ = NetworkTables.getTable(__pipe_name__)
 NetworkTables.initialize()
 camera_server = CameraServer.getInstance()
-
-#get all valid caps
-#os.system("v4l2-ctl -c exposure=6")
-#os.system("v4l2-ctl --set-fmt-video=width=160,height=120,pixelformat=BGR")
-cap1 = cv2.VideoCapture(-1)
-cap1.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
-cap1.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
-cap1.set(cv2.CAP_PROP_EXPOSURE,6)
+HOST = socket.gethostname()
+TCP_IP = socket.gethostbyname(HOST)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((TCP_IP, 5000))
+if __name__ == "__main__":
+  #get all valid caps
+  #os.system("v4l2-ctl -c exposure=6")
+  #os.system("v4l2-ctl --set-fmt-video=width=160,height=120,pixelformat=BGR")
+  cap1 = cv2.VideoCapture(-1)
+  cap1.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+  cap1.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
+  cap1.set(cv2.CAP_PROP_EXPOSURE,6)
 def __get_ports__():
   ports = []
   cap = cv2.VideoCapture(-1) 
@@ -89,6 +94,7 @@ def __get_ports__():
   return ports
 
 #get video for cameras and apply settings
+
 def __get_cap__(id, index):
   _camera_ = camera_server.startAutomaticCapture(dev=id, return_server=False)
   try:
@@ -100,28 +106,27 @@ def __get_cap__(id, index):
   _camera_.setFPS(fps)
   _camera_.setPixelFormat(cscore.VideoMode.PixelFormat.kYUYV)
   return _camera_
+if __name__ == "__main__":
+  for index, id in enumerate(__get_ports__()):
+    cameras.append(__get_cap__(id, index))
 
-for index, id in enumerate(__get_ports__()):
-  cameras.append(__get_cap__(id, index))
-
-__index__ = int(__pipeline__.getNumber('cap_number', 1))
-__image__ = numpy.zeros(shape=(width, height, 3), dtype=numpy.uint8)
-try:
-  #loads grip pipeline and user script
-  with open(__config_path__ + '/grips/' + config['grip_file']) as grip_file:
-    exec("".join(i for i in grip_file.readlines()))
-    grip_pipe = eval('GripPipeline()')
-  with open(__config_path__ + '/scripts/' + config['script_file']) as script_file:
-    exec("".join(i for i in script_file.readlines())) 
-except Exception as e:
-  print("error loading grip file or user script")
-  print(e)
+  __index__ = int(__pipeline__.getNumber('cap_number', 1))
+  __image__ = numpy.zeros(shape=(width, height, 3), dtype=numpy.uint8)
+  try:
+    #loads grip pipeline and user script
+    with open(__config_path__ + '/grips/' + config['grip_file']) as grip_file:
+      exec("".join(i for i in grip_file.readlines()))
+      grip_pipe = eval('GripPipeline()')
+    with open(__config_path__ + '/scripts/' + config['script_file']) as script_file:
+      exec("".join(i for i in script_file.readlines())) 
+  except Exception as e:
+    print("error loading grip file or user script")
+    print(e)
 
 def get_video(index, image):
   return camera_server.getVideo(camera=cameras[index]).grabFrame(image)[1]
-  
-prev,nex,conf,pt,done = False,False,False,False,False
-def process_Init(index):
+s.settimeout(0.01)
+def process_Init():
     s_time = time.time()
     counter,prev = 1,1
     source = cscore.CvSource("conf_stream",cscore.VideoMode(cscore.VideoMode.PixelFormat(cscore.VideoMode.PixelFormat.kMJPEG),width,height,fps))
@@ -130,55 +135,83 @@ def process_Init(index):
     with open("/root/.hawk/pipelines/grip_i.py","r") as grip_file:
           exec("".join(i for i in grip_file.readlines()))
           grip_pipe = eval('GripPipeline()')
-    while not False:#done:
+    while not done:
         try:
-          #adjust prev/next
-          if prev:
-            counter -=1
-            if counter <= 0:
-              counter = 1
-          if nex:
-            counter += 1
-            if counter > 4:
-              counter = 4
-          changed = prev or nex 
-          #input("Press Enter to continue...") 
-          if changed:
-              with open("/root/.hawk/pipelines/grip_" + (counter * 'i') + '.py') as grip_file:
-                exec("".join(i for i in grip_file.readlines()))
-                grip_pipe = eval('GripPipeline()')
-          #get image from original stream
-          pic = cap1.read()[1]
-          cv2.cvtColor(pic,84)
-          #process and get rgb threshold
-          grip_pipe.process(pic)
-          pic = grip_pipe.rgb_threshold_output
-          #publish to a new stream
-          source.putFrame(pic)
-          print("config running for " + str(round(time.time() - s_time,1)) + " seconds",end='\r')
+          #update vars
+          try:
+            #blockPrint()
+            data = s.recvfrom(20)[0] 
+            changed_vals(data)
+            #enablePrint()
+          finally:
+            if not conf:
+            #adjust prev/next
+              if prev:
+                prev = False
+                counter -=1
+                if counter <= 0:
+                  counter = 1
+              if nex:
+                nex = False
+                counter += 1
+                if counter > 4:
+                  counter = 4
+              #input("Press Enter to continue...") 
+              if prev or nex:
+                  with open("/root/.hawk/pipelines/grip_" + (counter * 'i') + '.py') as grip_file:
+                    exec("".join(i for i in grip_file.readlines()))
+                    grip_pipe = eval('GripPipeline()')
+            #get image from original stream
+            pic = cap1.read()[1]
+            cv2.cvtColor(pic,84)
+            #process and get rgb threshold
+            grip_pipe.process(pic)
+            pic = grip_pipe.rgb_threshold_output
+            #publish to a new stream
+            source.putFrame(pic)
+            print("config running for " + str(round(time.time() - s_time,1)) + " seconds",end='\r')
         except Exception as e:
           print(e)
-      
+#TODO: find a better solution!!
+def changed_vals(val):
+  print(val)
+  if not val: return
+  if val == "b'PREV'":
+    prev = True
+    return
+  if val == "b'NEX'":
+    nex = True
+    return
+  if val == "b'SET_CONF'":
+    conf = True
+    return
+  if val == "b'ADD_PT'":
+    pt = True
+    return
+  Done = True #will get here only if nothing left
+# Disable
+def blockPrint():
+    sys.stdout = open(os.devnull, 'w')
 
+# Restore
+def enablePrint():
+    sys.stdout = sys.__stdout__
+if __name__ == "__main__":
+  process_Init()
+                  
+  input("Press Enter to continue...")
+  while True:
+    try:
+                  __index__ = int(__pipeline__.getNumber('cap_number', 1))
+                  
+                  if __index__ != -1:
+                          
+                          __image__ = get_video(__index__, __image__)
+                          grip_pipe.process(__image__)
+                          counter = counter + 1 #change r u fucking dumb
+                  processor(image, grip_pipe.filter_contours_output or [], (counter > 10))
+                  if counter > 10:
+                            counter = 0
+    except Exception as e:
+                  error(e, __pipeline__)
 
-counter = 0
-for x in range(1,4):
-  process_Init(__index__)
-                
-input("Press Enter to continue...")
-while True:
-  try:
-                __index__ = int(__pipeline__.getNumber('cap_number', 1))
-                if __index__ != -1:
-                        
-                        __image__ = get_video(__index__, __image__)
-                        grip_pipe.process(__image__)
-                        counter = counter + 1 #change r u fucking dumb
-                processor(image, grip_pipe.filter_contours_output or [], (counter > 10))
-                if counter > 10:
-                          counter = 0
-  except Exception as e:
-                error(e, __pipeline__)
-
-def changed_vals(prevV,nexV,confV,ptV,doneV):
-  prev,nex,conf,pt,done = prevV,nexV,confV,ptV,doneV
